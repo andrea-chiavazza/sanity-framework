8edbe69abd765f796d70715647c67018fc241a28
Andrea Chiavazza <ndrchvzz@gmail.com>
2013-11-28 08:09:15 +0000

    make MenuActionMap extend LinkedHashMap

--- src/main/java/func/utility/swing/MenuActionMap.java
+++ src/main/java/func/utility/swing/MenuActionMap.java
@@ -1,14 +1,14 @@
 package func.utility.swing;
 
 import javax.swing.*;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * User: andrea
  * Date: 20/11/13
  * Time: 09:59
  */
-public class MenuActionMap extends HashMap<String,Action> {
+public class MenuActionMap extends LinkedHashMap<String,Action> {
     public Action put(String key,
                       Action value) {
         value.putValue(Action.NAME, key);
